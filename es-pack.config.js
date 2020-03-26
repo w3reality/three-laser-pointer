7f9dc94357bb129853b438d064480575a28b32a4
j-devel <j@w3reality.com>
2020-03-26 21:56:17 +0700

    Fix the 'THREE' symlink issue that was failing umd 'verify' on linux (hence CI)

--- es-pack.config.js
+++ es-pack.config.js
@@ -7,6 +7,13 @@ module.exports = {
         webpackConfig.output.library = 'Laser';
     },
     onVerify: (preloadJs) => {
+        const pathUpper = path.resolve(__dirname, './node_modules/THREE');
+        if (!fs.existsSync(pathUpper)) { // can be true on linux (case-sensitive fs)
+            // ├── node_modules
+            // │   └── THREE -> ./three
+            try { fs.symlinkSync('./three', pathUpper); } catch (_) {}
+        }
+
         preloadJs.node = path.resolve(__dirname, './tests/node/preload.js');
         preloadJs.browser = path.resolve(__dirname, './node_modules/three/build/three.min.js');
     },
